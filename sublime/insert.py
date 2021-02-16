7ff6843a908d52e308763d6f5ee974c0a7b70bf0
zwx2238 <61327648+zwx2238@users.noreply.github.com>
2021-02-16 12:55:38 +0800

    Delete sublime directory

--- sublime/insert.py
+++
@@ -1,7 +0,0 @@
-import sublime
-import sublime_plugin
-
-
-class InsertGreaterThanCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		self.view.insert(edit, self.view.sel()[0].begin(), " >")
