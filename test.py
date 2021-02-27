c061fa9767f973fab08a7483647677fd4a521cd0
zwx2238 <2238761683@qq.com>
2021-02-27 10:45:49 +0800

    '12'

---
+++ test.py
@@ -0,0 +1,8 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+@app.route('/')
+def index():
+	return render_template('index.html',name='hisasdf')
+
+if __name__ == '__main__':
+	app.run(debug=True,port="1000")
